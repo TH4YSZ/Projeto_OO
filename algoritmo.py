b88be972880eb4b2e6d54f280740b393c8c7bf6c
TH4YSZ <thainacristina.sz07@gmail.com>
2023-08-10 15:48:54 -0300

    Criação do menu

--- algoritmo.py
+++ algoritmo.py
@@ -1,7 +1,40 @@
-from classes import*
+from classe_cliente import*
+import os
 
 y = 0
 
+
+banco = Banco("MMALT-PAY")
 #Menu
 while y == 0:
-    menu = int(input("BEM-VINDO AO PIXPAY! \n \n [1] Conta \n [2] Cadastro \n [3] Transação \n [4] Transferência \n [5] Sair \n \n Digite a opção desejada: "))
+    try:
+        menu = int(input("\n BEM-VINDO AO MMALT-PAY! \n \n [1] Conta \n [2] Cadastro \n [3] Sair \n \n Digite a opção desejada: "))
+        match menu:
+            case 2:
+                try:
+                    os.system("cls")
+                    nome = input("Digite nome e sobrenome: ")
+                    telefone = input("Digite o número de telefone: ")
+                    email = input("Digite o email: ")
+                    senha = input("Digite a senha: ")
+                    cliente = Cliente(nome, telefone, email, senha)
+                    banco.adicionar_cliente(nome, telefone, email, senha)
+
+                    print("Cliente cadastrado com sucesso!")
+                    os.system("pause")
+                    os.system("cls")
+                except Exception in erro:
+                    op_invalida()
+            case 3:
+                y = 1
+            
+            case _:
+                op_invalida()
+
+    except Exception as erro:
+        op_invalida()
+
+def op_invalida():
+    print("\n Opção inválida. \n")
+    os.system("pause")
+    os.system("cls")
