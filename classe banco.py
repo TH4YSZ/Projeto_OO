cb73394c3e2f50d263ed092e47ef1b3e2cc239b4
lerpaiva <leticiarpaival@gmail.com>
2023-08-10 16:01:10 -0300

    upload conf

--- classe banco.py
+++ classe banco.py
@@ -1,3 +1,11 @@
 class banco:
+    def __init__(self, nome, idade, telefone, email, cpf, senha):
+        self._nome = nome
+        self._idade = idade
+        self._telefone = telefone
+        self._email = email
+        self._cpf = cpf
+        self._senha = senha
 
-
+    def cadastrar(self,_nome,_idade,_telefone,_email,_cpf, _senha):
+        clientes = banco(_nome,_idade,_telefone, _email,_cpf,_senha)
