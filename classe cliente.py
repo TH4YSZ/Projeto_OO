110eaa99fd1770dc559700c07033054cebce894d
TH4YSZ <thainacristina.sz07@gmail.com>
2023-08-10 16:05:08 -0300

    Criação das classes e do menu

--- classe cliente.py
+++
@@ -1,7 +0,0 @@
-class Cliente:
-    def __init__(self, nome, senha, telefone, email):
-        self.nome = nome
-        self.senha = senha
-        self.telefone = telefone
-        self.email = email
-
