94ee96923221eb5b72a60a33fb3fedb5bec2de60
lerpaiva <leticiarpaival@gmail.com>
2023-08-17 07:37:27 -0300

    upload metodo

--- classe banco.py
+++ classe banco.py
@@ -1,4 +1,4 @@
-class banco:
+class Banco:
     def __init__(self, nome, idade, telefone, email, cpf, senha):
         self._nome = nome
         self._idade = idade
@@ -6,6 +6,34 @@ class banco:
         self._email = email
         self._cpf = cpf
         self._senha = senha
+        self.clientes={}
+        
+    def cadastrar(self,nome,idade,telefone,email,cpf, senha):
+        
+        print("Para cadastrar um cliente, preencha as informações.")
+        print()
+        self.clientes = input({Banco( "Nome:",nome,"Idade:",idade,"Telefone:",telefone,"Email:", email,"CPF:",cpf, "Senha:",senha)})
+        return self.clientes
+    
+    def excluir_conta(self,senha,cpf, clientes):
+        print ("Para excluir sua conta, preencha as seguintes informações:")
+        x = int (input("Cpf:"))
+        y = int (input("Senha:"))
+        if cpf in clientes:
+            if senha in clientes:
+                del clientes
+            else:
+                print ("Senha Inválida")
+        else:
+            print("CPF Inválido")
 
-    def cadastrar(self,_nome,_idade,_telefone,_email,_cpf, _senha):
-        clientes = banco(_nome,_idade,_telefone, _email,_cpf,_senha)
+    def alterar_conta(self, senha, cpf, clientes1):
+        x = int (input("Cpf:"))
+        y = int (input("Senha:"))
+        if cpf in clientes1:
+            if senha in clientes1:
+                 clientes1
+            else:
+                print ("Senha Inválida")
+        else:
+            print("CPF Inválido")
